bfa825c1aff4ddb2fe5e2316d2ca00d8549d8a7f
Lucas <lucasdamiangnzlz@gmail.com>
2025-08-18 21:09:22 -0300

    Agrega funciones para ocultar los formularios

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -12,3 +12,13 @@ export function mostrarFormularioInicioDeSesion() {
 	const $contenedorFormularioInicioDeSesion = document.querySelector(".contenedor-formulario-inicio-de-sesion");
 	$contenedorFormularioInicioDeSesion.id = "";
 }
+
+export function ocultarFormularioDeCrearCuenta() {
+	const $contenedorFormularioCrearCuenta = document.querySelector(".contenedor-formulario-crear-cuenta");
+	$contenedorFormularioCrearCuenta.id = "oculto";
+}
+
+export function ocultarFormularioDeinicioDeSesion() {
+	const $contenedorFormularioInicioDeSesion = document.querySelector(".contenedor-formulario-inicio-de-sesion");
+	$contenedorFormularioInicioDeSesion.id = "oculto";
+}
