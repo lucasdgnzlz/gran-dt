210dc996e9e9037b165babc3c298c97a18010a2a
Lucas <lucasdamiangnzlz@gmail.com>
2025-08-18 21:07:02 -0300

    Agrega fuciones para completar el otro formulario
    - Añade funcionalidad para volver al otro formulario en caso de querer
      registrarse o iniciar sesión

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /* Inicio página */
 
-import {validarFormularioInicioSesion, validarFormularioRegistro} from "./validaciones/validaciones.js";
-import {ocultarBotonesInicioPagina, mostrarFormularioCrearCuenta, mostrarFormularioInicioDeSesion} from "./ui/ui.js";
+import { validarFormularioInicioSesion, validarFormularioRegistro } from "./validaciones/validaciones.js";
+import { ocultarBotonesInicioPagina, mostrarFormularioCrearCuenta, ocultarFormularioDeCrearCuenta, mostrarFormularioInicioDeSesion, ocultarFormularioDeinicioDeSesion } from "./ui/ui.js";
+import { ocultarErroresFormularioRegistro, ocultarErroresFormularioInicioSesion } from "./ui/errores-formularios.js";
 
 const $botonCrearCuenta = document.querySelector(".boton-crear-cuenta");
 const $botonYaTengoCuenta = document.querySelector(".boton-ya-tengo-cuenta");
@@ -28,11 +29,13 @@ $formularioRegistro.addEventListener("submit", (e) => {
 	const datos = {
 		email: datosFormulario.get("email"),
 		contrasenaRegistro: datosFormulario.get("contrasena-registro"),
-		contrasenaRegistroConfirmacion: datosFormulario.get("contrasena-registro-confirmacion")
+		contrasenaRegistroConfirmacion: datosFormulario.get("contrasena-registro-confirmacion"),
 	};
 
 	validarFormularioRegistro(datos);
 });
+
+/* Inicio de sesión */
 
 const $formularioInicioDeSesion = document.querySelector(".formulario-inicio-de-sesion");
 
@@ -43,8 +46,32 @@ $formularioInicioDeSesion.addEventListener("submit", (e) => {
 
 	const datos = {
 		email: datosFormulario.get("email"),
-		contrasenaInicioSesion: datosFormulario.get("contrasena-inicio-sesion"),
+		contrasenaInicioSesion: datosFormulario.get("contrasena-inicio-sesion")
 	};
 
 	validarFormularioInicioSesion(datos);
+});
+
+/* Consultas de los formularios */ // Entiéndase como "¿ya sos usuario? iniciá sesión, etc"
+
+const $consultaInicioSesion = document.querySelector(".consulta-inicio-sesion");
+
+$consultaInicioSesion.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	ocultarBotonesInicioPagina($botonCrearCuenta, $botonYaTengoCuenta);
+	mostrarFormularioInicioDeSesion();
+	ocultarFormularioDeCrearCuenta();
+	ocultarErroresFormularioInicioSesion(["email", "contrasena"]);
+});
+
+const $consultaRegistro = document.querySelector(".consulta-registro");
+
+$consultaRegistro.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	ocultarBotonesInicioPagina($botonCrearCuenta, $botonYaTengoCuenta);
+	mostrarFormularioCrearCuenta();
+	ocultarFormularioDeinicioDeSesion();
+	ocultarErroresFormularioRegistro(["email", "contrasena", "contrasena-confirmacion"]);
 });
