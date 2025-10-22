d00bdc5ffc079229d9f34b411897b3b11e151fd3
Lucas <lucasdamiangnzlz@gmail.com>
2025-10-22 20:24:28 -0300

    Agrega test para inicio de sesión fallido

--- cypress/e2e/inicio-de-sesion-registro.cy.js
+++ cypress/e2e/inicio-de-sesion-registro.cy.js
@@ -3,38 +3,48 @@ context("Inicio de sesión", () => {
     cy.visit("/"); // URL ya definida en la configuración de Cypress
   });
 
+  const datosAccesoPrueba = {
+    email: "ejemploemail@gmail.com",
+    contrasenaInicioSesion: "ejemplocontrasena1234",
+  };
+
   it("Inicio correcto de sesión", () => {
-    const datosAccesoPrueba = {
-      email: "ejemploemail@gmail.com",
-      contrasenaInicioSesion: "ejemplocontrasena1234",
-    };
+    cy.get(".boton-ya-tengo-cuenta").should("be.visible").should("have.text", "Ya tengo cuenta").click();
 
-    cy.get(".boton-ya-tengo-cuenta")
-			.should("be.visible")
-			.should("have.text", "Ya tengo cuenta")
-			.click();
+    cy.get(".formulario-inicio-de-sesion").should("be.visible");
 
-    cy.get(".formulario-inicio-de-sesion")
-			.should("be.visible");
+    cy.get("#email-inicio-sesion").should("be.visible").type(datosAccesoPrueba.email);
 
-    cy.get("#email-inicio-sesion")
-			.should("be.visible").type(datosAccesoPrueba.email);
+    cy.get("#contrasena-inicio-sesion").should("be.visible").type(datosAccesoPrueba.contrasenaInicioSesion);
 
-		cy.get("#contrasena-inicio-sesion")
-			.should("be.visible")
-			.type(datosAccesoPrueba.contrasenaInicioSesion);
+    cy.get(".mensaje-de-bienvenida").should("not.be.visible");
 
-		cy.get(".mensaje-de-bienvenida")
-			.should("not.be.visible");
+    cy.get(".boton-iniciar-sesion").should("be.visible").click();
 
-		cy.get(".boton-iniciar-sesion")
-			.should("be.visible")
-			.click();
+    cy.get(".mensaje-de-bienvenida").should("be.visible");
 
-		cy.get(".mensaje-de-bienvenida")
-			.should("be.visible");
+    cy.get(".contenedor-formulario-inicio-de-sesion").should("not.be.visible");
+  });
 
-		cy.get(".contenedor-formulario-inicio-de-sesion")
-			.should("not.be.visible");
+  it("Inicio de sesión fallido", () => {
+    cy.get(".boton-ya-tengo-cuenta").should("be.visible").and("have.text", "Ya tengo cuenta").click();
+
+    cy.get(".formulario-inicio-de-sesion").should("be.visible");
+
+    cy.get("#email-inicio-sesion").should("be.visible").type("mailnocorrecto@ejemplo.com");
+    cy.get("#contrasena-inicio-sesion").should("be.visible").type("contrasena123");
+
+    cy.get(".mensaje-de-bienvenida").should("not.be.visible");
+    cy.get(".mensaje-error-email-inicio-sesion").should("not.be.visible");
+    cy.get(".mensaje-error-contrasena-inicio-sesion").should("not.be.visible");
+
+    cy.get(".boton-iniciar-sesion").should("be.visible").click();
+
+    cy.get(".mensaje-de-bienvenida").should("not.be.visible");
+    cy.get(".mensaje-error-email-inicio-sesion").should("be.visible").and("have.text", "Email inválido");
+    cy.get(".mensaje-error-contrasena-inicio-sesion").should("be.visible").and("have.text", "Contraseña inválida");
+
+    cy.get("#email-inicio-sesion").should("be.visible").and("have.class", "is-invalid");
+    cy.get("#contrasena-inicio-sesion").should("be.visible").and("have.class", "is-invalid");
   });
 });
